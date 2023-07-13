5ed19683325fb40a214dce0f407f347bd5fc1d44
Max Ghenis <mghenis@gmail.com>
2023-07-13 13:31:02 -0400

    Add children

--- app.py
+++ app.py
@@ -8,23 +8,35 @@ from policyengine_us import Simulation
 
 
 def get_net_incomes(
-    state_code, head_employment_income, spouse_employment_income
+    state_code, head_employment_income, spouse_employment_income, children=0
 ):
     # Tuple of net income for separate and married.
     net_income_married = get_net_income(
-        state_code, head_employment_income, spouse_employment_income
+        state_code,
+        head_employment_income,
+        spouse_employment_income,
+        children=children,
     )
-    net_income_head = get_net_income(state_code, head_employment_income)
-    net_income_spouse = get_net_income(state_code, spouse_employment_income)
-    return net_income_married, net_income_head + net_income_spouse
+    # Assume the head, not spouse, has the children.
+    net_income_head = get_net_income(
+        state_code, head_employment_income, children=children
+    )
+    net_income_spouse = get_net_income(
+        state_code, spouse_employment_income, children=0
+    )
+    return net_income_married, net_income_head, net_income_spouse
 
 
 DEFAULT_AGE = 40
+DEFAULT_CHILD_AGE = 10
 
 
 # Create a function to get net income for household
 def get_net_income(
-    state_code, head_employment_income, spouse_employment_income=None
+    state_code,
+    head_employment_income,
+    spouse_employment_income=None,
+    children=0,
 ):
     # Start by adding the single head.
     situation = {
@@ -43,10 +55,20 @@ def get_net_income(
         }
         # Add your partner to members list.
         members.append("your partner")
+    # Add one child as a person for each child using a for loop.
+    for i in range(children):
+        situation["people"][f"child {i}"] = {
+            "age": {"2023": DEFAULT_CHILD_AGE}
+        }
+        # Add child to members list.
+        members.append(f"child {i}")
+        print(i)
     # Create all parent entities.
     situation["families"] = {"your family": {"members": members}}
     situation["marital_units"] = {"your marital unit": {"members": members}}
-    situation["tax_units"] = {"your tax unit": {"members": members}}
+    situation["tax_units"] = {
+        "your tax unit": {"members": members, "premium_tax_credit": 0}
+    }
     situation["spm_units"] = {"your spm_unit": {"members": members}}
     situation["households"] = {
         "your household": {
@@ -64,11 +86,14 @@ def get_net_income(
 state_code = st.text_input("State Code", "CA")
 head_employment_income = st.number_input("Head Employment Income", 0)
 spouse_employment_income = st.number_input("Spouse Employment Income", 0)
+children = st.number_input("Number of Children", 0)
 
 # Get net incomes.
-net_income_married, net_income_separate = get_net_incomes(
-    state_code, head_employment_income, spouse_employment_income
+net_income_married, net_income_head, net_income_spouse = get_net_incomes(
+    state_code, head_employment_income, spouse_employment_income, children
 )
+
+net_income_separate = net_income_head + net_income_spouse
 
 # Determine marriage penalty or bonus, and extent in dollars and percentage.
 marriage_bonus = net_income_married - net_income_separate
@@ -78,6 +103,8 @@ marriage_bonus_percent = marriage_bonus / net_income_married
 # Display net incomes in Streamlit.
 st.write("Net Income Married: ", net_income_married)
 st.write("Net Income Separate: ", net_income_separate)
+st.write("- Head: ", net_income_head)
+st.write("- Spouse: ", net_income_spouse)
 
 # Display marriage bonus or penalty in Streamlit as a sentence.
 # For example, "You face a marriage [PENALTY/BONUS]"
