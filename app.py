e0ec66ff47cca6c4d25295f45cee069dc2b88f91
Emie Elvire Sabumukama <67650132+emieelvire@users.noreply.github.com>
2024-03-14 19:11:49 +0000

    resolve merge conflict

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import math
-#
+import plotly.express as px
 from policyengine_us import Simulation
-
-
+from policyengine_core.charts import format_fig
 # Create a function to get net income for the household, married or separate.
-
 
 def get_net_incomes(state_code, head_employment_income, spouse_employment_income, children_ages = {}):
     # Tuple of net income for separate and married.
@@ -134,6 +132,7 @@ def get_net_income(state_code, head_employment_income, spouse_employment_income=
     situation["households"] = {
         "your household": {"members": members, "state_name": {"2023": state_code}}
     }
+  
 
     simulation = Simulation(situation=situation)
 
@@ -147,8 +146,8 @@ repo_link = st.markdown("This application utilizes the policyengine API <a href=
 
 # Create Streamlit inputs for state code, head income, and spouse income.
 state_code = st.text_input("State Code", "CA")
-head_employment_income = st.number_input("Head Employment Income", 0)
-spouse_employment_income = st.number_input("Spouse Employment Income", 0)
+head_employment_income = st.number_input("Head Employment Income", step=20000, value=0)
+spouse_employment_income = st.number_input("Spouse Employment Income", step=10000, value=0)
 num_children = st.number_input("Number of Children", 0)
 children_ages = {}
 for num in range(1,num_children + 1):
@@ -180,11 +179,6 @@ if submit:
     st.write("Net Income Married: ", net_income_married)
     st.write("Net Income Not Married: ", net_income_separate)
 
-    # Display marriage bonus or penalty in Streamlit as a sentence.
-    # For example, "You face a marriage [PENALTY/BONUS]"
-    # "If you file separately, your combined net income will be [X] [more/less] (y%) than if you file together."
-
-
     def summarize_marriage_bonus(marriage_bonus):
         # Create a string to summarize the marriage bonus or penalty.
         return (
@@ -203,15 +197,130 @@ if submit:
 
     st.write(summarize_marriage_bonus(marriage_bonus))
     # Sample data
-    data = {
+    table_data = {
         'Program': programs,
         'Married': married_programs,
         'Not Married': separate,
         'Delta ': delta
     }
 
-    # Create a DataFrame
-    #df = pd.DataFrame(data)
-
     # Display the table in Streamlit
-    st.table(data)
+    st.table(table_data)
+
+
+    def check_child_influence():
+        salary_ranges = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000]
+        data = []
+
+        for i in range(len(salary_ranges)):
+            temp_data = []
+            
+            for j in range(len(salary_ranges)):
+                head_employment_income = salary_ranges[i]
+                spouse_employment_income = salary_ranges[j]
+
+                # Assuming get_net_incomes now returns a tuple (net_income_married, net_income_head_spouse)
+                net_income_married,  net_income_separate  = get_net_incomes(
+                    state_code, head_employment_income, spouse_employment_income
+                )
+                marriage_bonus = net_income_married - net_income_separate
+                if marriage_bonus >= 0:
+                    if marriage_bonus > 5000:   
+                        temp_data.append(1)
+                    elif marriage_bonus > 3000:
+                        temp_data.append(0.9)
+                    elif marriage_bonus > 1000:
+                        temp_data.append(0.8)
+                    elif marriage_bonus > 500:
+                        temp_data.append(0.7)
+                    elif marriage_bonus > 100:
+                        temp_data.append(0.6)
+                    else:
+                        temp_data.append(0.5)
+                else:
+                    if marriage_bonus < -5000:   
+                        temp_data.append(0)
+                    elif marriage_bonus < -3000:
+                        temp_data.append(0.1)
+                    elif marriage_bonus < -1000:
+                        temp_data.append(0.2)
+                    elif marriage_bonus < -500:
+                        temp_data.append(0.3)
+                    elif marriage_bonus < -100:
+                        temp_data.append(0.4)
+                    else:
+                        temp_data.append(0.5)
+        
+            data.append(temp_data)
+
+        return data
+
+        
+    def get_chart():
+    # Function to calculate the input data (replace with your actual data calculation)
+        # Set numerical values for x and y axes
+        x_values = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000]
+        y_values = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000]
+
+        # Display loading spinner while calculating data
+        with st.spinner("Calculating Heatmap... May take 90 seconds"):
+            # Calculate data (replace with your actual data calculation)
+            data = check_child_influence()
+        color_scale = [
+    (0, '#616161'),
+    (0.1, '#757575'),
+    (0.2, '#8A8A8A'),
+    (0.3, '#9E9E9E'),
+    (0.4, '#B3B3B3'),
+    (0.5, '#CCCCCC'),
+    (0.6, '#A4C2D5'),
+    (0.7, '#6E93B7'),
+    (0.8, '#456B9C'),
+    (0.9, '#2C6496'),
+    (1, '#2C6496')
+]
+        
+        # Display the chart once data calculation is complete
+        fig = px.imshow(data,
+                        labels=dict(x="Head Employment Income", y="Spouse Employment Income", color="Bonus"),
+                        x=x_values,
+                        y=y_values,
+                        origin='lower'
+                    )
+
+        fig.update_xaxes(side="bottom")
+        fig.update_coloraxes(
+            showscale=True,
+            colorscale = color_scale
+        )
+        fig.update_layout(
+            xaxis=dict(
+                tickmode='array',
+                tickvals=[10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000],
+                ticktext=["{}k".format(int(val/1000)) for val in [10000,20000, 30000,40000,50000, 60000, 70000, 80000]],
+                showgrid=True,
+                zeroline=False,
+                title=dict(text='Head Employment Income', standoff=15),
+            ),
+            yaxis=dict(
+                tickmode='array',
+                tickvals=[10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000],
+                ticktext=["{}k".format(int(val/1000)) for val in [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000]],
+                showgrid=True,
+                zeroline=False,
+                title=dict(text='Spouse Employment Income', standoff=15),
+                scaleanchor="x",
+                scaleratio=1,
+            )
+        )
+        fig.update_layout(height=600, width=800)
+        # Add header
+        st.markdown("<h3 style='text-align: center; color: black;'>Marriage Incentive and Penalty Analysis</h3>", unsafe_allow_html=True)
+        fig = format_fig(fig)
+        # Display the chart
+        
+        st.plotly_chart(fig, use_container_width=True)
+    
+    get_chart()
+
+
